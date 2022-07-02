1a9a08bef20cb968cef88d12b5d0e01b770f1a6f
KE XUE <73022231+xueke414@users.noreply.github.com>
2022-07-02 15:55:40 +0800

    Update mysql.js

--- Middleware/mysql.js
+++ Middleware/mysql.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const secret = require("../secret/secrett/secret");
+const secret = require("../secret/secret");
 const db = new Sequelize(...secret.sequelize);
 
 
